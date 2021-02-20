e4876b4705a91f441e2fdd0e907095a1a56fd86f
Alexandre Brossette <alexandrebrossette1@gmail.com>
2021-02-20 13:04:33 +0100

    +

--- srcs/launcher.sh
+++ srcs/launcher.sh
@@ -8,7 +8,7 @@ chmod -R 755 /var/www/*
 
 #SSL
 mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=chibraxomax/CN=localhost"
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/_.pem -keyout /etc/nginx/ssl/_.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=alebross/CN=_"
 
 #MARIADB
 service mysql start
